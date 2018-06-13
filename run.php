486ee2c8544583eb4d1fd20df49fcdf77b26f326
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-06-13 13:50:30 +0100

    print all request on run.php

--- run.php
+++ run.php
@@ -1,21 +1,5 @@
 <?php
-error_reporting(-1);
 
-$url = "http://gtweb.gtbank.com/CustomerDetails/Api/CustomerDetails";
-$token = "3490oihtbgbgutt9";
-$data = array('Nuban' =>'0130071685');
+print_r($_REQUEST);
 
-  $opts = array('http' => array(
-    'method' => 'POST',
-    'header' =>
-        "Authorization:".$token."\r\n".
-        "Content-type:  application/json\r\n".
-        "Content-Length: ".strlen($data)."\r\n".
-    'content' => $data,
-    );
-  $stream = stream_context_create($opts);
-  $fp = fopen($url, 'rb', false, $stream);
-  $reply = stream_get_contents($fp);
-
-echo "i am good";
  ?>
