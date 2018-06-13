417d79e0611be46af7bcfeed23ceff5a3e9f52f8
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-06-13 17:05:31 +0100

    update run test

--- run.php
+++ run.php
@@ -1,5 +1,13 @@
-<?php
 
-print_r($_REQUEST);
+ <p> You can now use the Access Token to query the customer endpoint (Postman works) </p>
+ <ul>
+  <li>Set the Acess Token in the header `Authorization`</li>
+  <li>Add a field `Nuban` with a valid GT Bank Account number as the valid</li>
+</ul>
+ <?php
+    $accessToken = $_REQUEST['AccessToken'];
+    echo "<div>Access Token: ".$accessToken."</div>";
+    print_r($_REQUEST);
+  ?>
 
- ?>
+ <div>Copyright (c) 2018 Copyright Holder All Rights Reserved. Accounteer Test</div>
