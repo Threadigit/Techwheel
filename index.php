67d6c87f8e45d63852d1c546730bdaa1c67e2e56
Adetuyi Tolu Emmanuel <toluadetuyi@Adetuyis-MacBook-Pro-2.local>
2019-01-16 17:23:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -254,7 +254,7 @@
 	<div class="steps landing__section" data-aos="fade-up">
 		<div class="container" data-aos="fade-down">
 				<a name="about" style="height: 0px;"></a>
-			<h2>TechWheel Team</h2>
+			<h2>Team</h2>
 			<p>Below are the young vibrants industrious innovators who are leading the Techwheel team</p>
 		</div>
 		<div class="container">
